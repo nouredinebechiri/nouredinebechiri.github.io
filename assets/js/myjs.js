11f3f62354cfa93456dd4ee322202732b003066d
Nouredine BECHIRI <48387994+nouredinebechiri@users.noreply.github.com>
2025-09-13 19:19:17 +0100

    Delete assets directory

--- assets/js/myjs.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-     $("#header").load("header.html",function(response,status, xhr) {});
-     $("#overview").load("overview.html",function(response,status, xhr) {});
-     $("#expertise").load("expertise.html",function(response,status, xhr) {});
-     $("#tfc").load("tfc.html",function(response,status, xhr) {});
-     $("#career-skills").load("career-skills.html",function(response,status, xhr) {});
-     $("#copyright").load("copyright.html",function(response,status, xhr) {});
-});
-
